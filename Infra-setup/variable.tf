27aa7e9d70d48e60aaa770a43f07ee7965389194
weg-coder-rohit <rohit.singh042499@gmail.com>
2024-04-09 22:57:37 +0530

    added AMi name for docker ubuntu instance

--- Infra-setup/variable.tf
+++ Infra-setup/variable.tf
@@ -7,7 +7,7 @@ variable "AMIS" {
   default = {
     Nexus_server = "ami-01387af90a62e3c01"
     Sonar_server = "ami-007020fd9c84e18c7"
-    Docker_server_ami = ""
+    Docker_server_ami = "ami-0b8b44ec9a8f90422"
   }
 }
 
