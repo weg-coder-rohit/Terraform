4624d89df5e57278e07d42f5fd1f2fc46fbf5430
weg-coder-rohit <rohit.singh042499@gmail.com>
2024-04-10 16:41:58 +0530

    created security group in main.tf file of vpc module

--- Project/modules/vpc/main.tf
+++ Project/modules/vpc/main.tf
@@ -1,4 +1,4 @@
-module "vpc_example_complete" {
+module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.7.1"
 
@@ -22,4 +22,101 @@ module "vpc_example_complete" {
   vpc_tags = {
     Name = "MetNet"
   }
+}
+
+resource "aws_security_group" "beanstalk_elb_sg" {
+  vpc_id      = module.vpc.vpc_id
+  name        = "beanstalk_elb_sg"
+  description = "Security Group for Beanstalk load balancer"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "Bastion_host_sg" {
+  name        = "Bastion_host_sg"
+  vpc_id      = module.vpc.vpc_id
+  description = "Security Group for Bastion Host"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.MYIP]
+  }
+}
+
+resource "aws_security_group" "beanstalk_instance_sg" {
+  name        = "beanstalk_instance_sg"
+  vpc_id      = module.vpc.vpc_id
+  description = "Security Group for Instances under Beanstlak"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.Bastion_host_sg.id]
+  }
+}
+
+resource "aws_security_group" "backend_services_sg" {
+  name        = "backend_services_sg"
+  vpc_id      = module.vpc.vpc_id
+  description = "Secuirty group for backend Services"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.beanstalk_instance_sg.id]
+  }
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    security_groups = [ aws_security_group.Bastion_host_sg.id ]
+  }
+}
+
+resource "aws_security_group_rule" "Secuirty_group_rule" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.backend_services_sg.id
+  source_security_group_id = aws_security_group.backend_services_sg.id
+  depends_on = [ aws_security_group.backend_services_sg ]
 }
