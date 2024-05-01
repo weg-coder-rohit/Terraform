d82cbc7397dbfd20ad7a6b4f9cb06c7739d8408f
weg-coder-rohit <rohit.singh042499@gmail.com>
2024-05-01 15:20:45 +0530

    changed he vpc module file
    Added a subnect accossiation for private subnet with route table
    which has internate gateway route to test the rollback feature

--- Infra-setup/module/vnet/main.tf
+++ Infra-setup/module/vnet/main.tf
@@ -109,3 +109,9 @@ resource "aws_route_table_association" "terraroute_table_association3" {
   subnet_id      = aws_subnet.PUBSUB3.id
   route_table_id = aws_route_table.Terra_first_VPC_routetable.id
 }
+
+
+resource "aws_route_table_association" "terraroute_table_association4" {
+  subnet_id      = aws_subnet.PRIVSUB1.id
+  route_table_id = aws_route_table.Terra_first_VPC_routetable.id
+}
