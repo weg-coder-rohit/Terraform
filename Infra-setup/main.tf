925881fb9bfba3a377766a200a719d843ef2a41d
weg-coder-rohit <rohit.singh042499@gmail.com>
2024-05-02 14:26:47 +0530

    commiting the changes of adding 3 instance with 3 diffrent type

--- Infra-setup/main.tf
+++ Infra-setup/main.tf
@@ -1,6 +1,6 @@
-module "vnet" {
-  source = "./module/vnet"
-}
+# module "vnet" {
+#   source = "./module/vnet"
+# }
 
 # module "instances" {
 #   source = "./module/instances"
@@ -18,4 +18,21 @@ module "vnet" {
 #   source = "./module/Jenkins-Server"
 # }
 
+# provider "aws" {
+#   region = "us-west-2"  # Replace with your desired AWS region
+# }
 
+# resource "aws_instance" "ec2_instances" {
+#   count = length(var.instance_types)
+
+#   ami           = "ami-12345678"  # Replace with your desired AMI ID
+#   instance_type = var.instance_types[count.index]
+#   key_name      = "your-key-pair"  # Replace with your key pair name
+#   security_groups = ["your-security-group-name"]  # Replace with your security group name
+#   subnet_id     = "your-subnet-id"  # Replace with your subnet ID
+# }
+
+# variable "instance_types" {
+#   type    = list(string)
+#   default = ["t2.micro", "t3.medium", "m5.large"]
+# }
